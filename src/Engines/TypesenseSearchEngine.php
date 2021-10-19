b62e3380cb9c417d3810b21f0b0e68f72355103b
kdevan <kaidevan@gmail.com>
2021-10-19 15:35:10 -0700

    Add default for Builder limit
    If the limit is not explicitly set on the Builder then we get the error, "Devloops\LaravelTypesense\Engines\TypesenseSearchEngine::buildSearchParams(): Argument #3 ($perPage) must be of type int, null given". Here's a fix that works but I'd understand if you have another way that works. I hardcoded the limit to 10 because that's the Typesense default for the per_page argument: https://typesense.org/docs/0.21.0/api/documents.html#arguments.

--- src/Engines/TypesenseSearchEngine.php
+++ src/Engines/TypesenseSearchEngine.php
@@ -132,17 +132,17 @@ class TypesenseSearchEngine extends Engine
     /**
      * @param \Laravel\Scout\Builder $builder
      * @param int                    $page
-     * @param int                    $perPage
+     * @param int|null               $perPage
      *
      * @return array
      */
-    private function buildSearchParams(Builder $builder, int $page, int $perPage): array
+    private function buildSearchParams(Builder $builder, int $page, ?int $perPage): array
     {
         $params = [
             'q'                   => $builder->query,
             'query_by'            => implode(',', $builder->model->typesenseQueryBy()),
             'filter_by'           => $this->filters($builder),
-            'per_page'            => $perPage,
+            'per_page'            => $perPage ?? 10,
             'page'                => $page,
             'highlight_start_tag' => $this->startTag,
             'highlight_end_tag'   => $this->endTag,
